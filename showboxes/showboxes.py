836c41b2f8c0954f36345cbf2e03966d06969199
David Mawdsley <david.mawdsley@manchester.ac.uk>
2016-11-16 17:35:59 +0000

    Plot multiple bounding boxes; coloured according to position in command line

--- showboxes/showboxes.py
+++ showboxes/showboxes.py
@@ -6,9 +6,21 @@ import csv
 import sys
 import numpy as np
 import pandas as pd
-# Want to read in all arguments >2 and put the csv file from each into a dictionary
 
-df=pd.read_csv(sys.argv[3], sep = ",", header = 0, index_col = 0,
+bbox_collection = {}
+
+colours =np.array([(255,0,0),
+           (0,255,0),
+           (0,0,255),
+           (255,255,255)])
+
+
+
+
+fileind = 0
+for infile in sys.argv[3:]:
+
+    bbox_collection[fileind]=pd.read_csv(infile, sep = ",", header = 0, index_col = 0,
                dtype = {'Frame':np.int32},
                names = ["Frame", "time", "actpt", "bbcx", "bbcy",
                         "bbw", "bbh", "bbr",
@@ -16,6 +28,13 @@ df=pd.read_csv(sys.argv[3], sep = ",", header = 0, index_col = 0,
                         "bb2x", "bb2y",
                         "bb3x", "bb3y",
                         "bb4x", "bb4y"])
+    fileind = fileind + 1
+
+
+##for bbk in bbox_collection.keys():
+##    print bbox_collection[bbk].index
+##
+##quit()
 
 
 WINDOW_NAME = 'Playback'
@@ -35,17 +54,24 @@ got, img = video.read()
 
 while got:
     
-    
-    if frame in df.index:
-        print frame
-        actbb = df.loc[frame]
-        cv2.rectangle(img, (actbb['bb2x'].astype(int), actbb['bb2y'].astype(int)),
-                      (actbb['bb2x'].astype(int) + actbb['bbw'].astype(int),
-                       actbb['bb2y'].astype(int) + actbb['bbh'].astype(int)), (255, 0, 0), 2)  
+    for bbk in bbox_collection.keys():
+        if frame in bbox_collection[bbk].index:
+            print frame, bbk
+            actbb = bbox_collection[bbk].loc[frame]
 
-    cv2.imshow(WINDOW_NAME, img)
+            #Need to draw rectangle as four lines, since may not have rotation = 0
+            #cv2.rectangle(img, (actbb['bb1x'].astype(int), actbb['bb1y'].astype(int)),
+            #          (actbb['bb3x'].astype(int), actbb['bb3y'].astype(int)),colours[bbk], 2)
+            # From Rob's code (note column 0 is used as the index)
+            for i in xrange(4):
+                n = (i + 4) * 2 - 1
+                m = (((i + 1) % 4) + 4) * 2 -1 
+                p1 = (actbb[n].astype(int), actbb[n+1].astype(int))
+                p2 = (actbb[m].astype(int), actbb[m+1].astype(int))
+                cv2.line(img, p1, p2, colours[bbk], 2)
 
 
+   # cv2.imshow(WINDOW_NAME, img)
     videoout.write(img)
 
         
