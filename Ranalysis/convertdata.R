fe39f2b1e7d9a57a1326cd76593dfb3aa761ace7
David Mawdsley <david.mawdsley@manchester.ac.uk>
2016-11-16 17:36:35 +0000

    Rscript to convert from opencv to Cpp-mt format

---
+++ Ranalysis/convertdata.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+
+#  Convert data of the form 
+# fame, x, y, w, h
+# to the format used by CppMT
+
+#https://www.r-bloggers.com/passing-arguments-to-an-r-script-from-command-lines/
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)==1) {
+  # default output file
+  args[2] = "out.txt"
+}
+
+source("~/opencv/abc-classifier/Ranalysis/functions.R")
+
+indata <- readfeature(args[1])
+
+generateCppMTcsv(indata, args[2])
+
