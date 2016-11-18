e3a6823540f3746fef018b727a1d44f94a1f55d7
David Mawdsley <david.mawdsley@manchester.ac.uk>
2016-11-18 12:45:11 +0000

    Rscript to put both bounding boxes on all particpants

---
+++ showboxes/annotateall.R
@@ -0,0 +1,51 @@
+library(stringr)
+
+
+checkpath <- function(indir){
+  if(!dir.exists(indir)){
+    stop(paste(indir, "does not exist"))
+  }
+  
+  if(is.na(str_match(indir, "/$"))){
+    indir <- paste0(indir, "/")
+  }
+  
+  return(indir)
+}
+
+
+
+options <- commandArgs(trailingOnly = TRUE)
+if(length(options) != 4){
+  stop("Expecting input video, output folders, face detection folder, object tracking folder ")
+}
+
+inputdir <- checkpath(options[1])
+outputdir <- checkpath(options[2])
+facedir <- checkpath(options[3])
+objectdir <- checkpath(options[4])
+
+
+videofiles <- list.files(inputdir, pattern = "P\\d+_video.mp4$")
+
+for(v in videofiles[1]){
+  rootname <- str_extract(v, "(P\\d+)")
+  outname <- paste0(rootname, "_bboxes.avi")
+  facename <- paste0("pface", rootname, ".csv")
+  objectname <- paste0(rootname, "_video.csv")
+  
+  
+  cmd <- paste0("python showboxes.py ",
+                inputdir, v, " ",
+                outputdir, outname, " ",
+                facedir, facename, " ",
+                objectdir, objectname
+  )
+  #print(cmd)
+  system(cmd)
+  
+  
+  
+  
+}
+
