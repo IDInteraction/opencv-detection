4131b11974e36205a586e916966fc793500bf7ad
David Mawdsley <david.mawdsley@manchester.ac.uk>
2016-11-24 11:13:53 +0000

    Add frame skipping to runall script

--- Ranalysis/runall.R
+++ Ranalysis/runall.R
@@ -6,23 +6,24 @@ checkpath <- function(indir){
 #  if(!dir.exists(indir)){
 #    stop(paste(indir, "does not exist"))
 #  }
-  
+
   if(is.na(str_match(indir, "/$"))){
     indir <- paste0(indir, "/")
   }
-  
+
   return(indir)
 }
 
 
 
 options <- commandArgs(trailingOnly = TRUE)
-if(length(options) != 2){
-  stop("Expecting input and output folders")
+if(length(options) != 3){
+  stop("Expecting input, output and frameskip folders")
 }
 
 inputdir <- checkpath(options[1])
 outputdir <- checkpath(options[2])
+skipdir <- checkpath(options[3])
 
 
 videofiles <- list.files(inputdir, pattern = "P\\d+_video.mp4$")
@@ -32,14 +33,15 @@ for(v in videofiles){
   rootname <- str_extract(v, "(P\\d+)")
   facename <- paste0(rootname, "face",  ".csv")
   eyename <- paste0(rootname, "eye", ".csv")
-  
+  skipname <- paste0(rootname, "_video.skip")
   cmd <- paste0("python ./opencv/abc-classifier/dynamic/face.py ",
                    inputdir, v, " ",
                    outputdir, facename, " ",
-                   outputdir, eyename)
-  
+                   outputdir, eyename, " ",
+                   skipdir, skipname)
+
   system(cmd)
-  
+
   # convert to Cpp-mt format
   convertscript <- "./opencv/abc-classifier/Ranalysis/convertdata.R"
   cmd <- paste0("Rscript ",
@@ -47,7 +49,6 @@ for(v in videofiles){
                 " ",
                 outputdir, facename, " ",
                 outputdir, facename)
-  system(cmd)                 
-  
-}
+  system(cmd)
 
+}
