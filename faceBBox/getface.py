7831de5e9e0448b77e681327a36e71261ff5e5db
David Mawdsley <david.mawdsley@manchester.ac.uk>
2016-12-05 09:53:39 +0000

    Use OpenCV functions to get / set video position, rather than counting / skipping frams

--- faceBBox/getface.py
+++ faceBBox/getface.py
@@ -19,21 +19,9 @@ video = cv2.VideoCapture(sys.argv[1])
 
 cv2.namedWindow(WINDOW_NAME)
 
-frame = 0
-
-if(len(sys.argv)==3):
-    skiptime = int(sys.argv[2])
-else:
-    skiptime = 0
-
-skipframe = skiptime / 20 # TODO - don't hard code
-got, img = video.read()
-while got:
-    frame = frame + 1
-    got, img = video.read()
-    if(frame > skipframe):
-        break
-
+if(len(sys.argv) > 2):
+    print >> sys.stderr, ("Skipping " + sys.argv[2] + " ms")
+    video.set(cv2.cv.CV_CAP_PROP_POS_MSEC, int(sys.argv[2]))
 
 got, img = video.read()
 ox = 0
@@ -41,18 +29,18 @@ oy = 0
 ow, oh, _ = img.shape
 #print ox, oy, ow, oh
 
-frame = frame - 1 # Since we advance at the start of the loop below
+
 
 while got:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray_sm = gray[oy:oh, ox:ow]
     #print gray_sm.shape
-    frame = frame + 1
+    frame = video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
 
 
     faces = face_cascade.detectMultiScale(gray_sm, 1.3, 5)
     for (x, y, w, h) in faces:
-        print(str(x) + "," + str(y) + "," + str(w) + "," + str(h) + "," + str(frame))
+        print(str(x) + "," + str(y) + "," + str(w) + "," + str(h) + "," + str(int(frame)))
         if(len(faces) > 1):
             print >> sys.stderr, (len(faces))
             print >> sys.stderr, ("WARNING, >1 face detected")
